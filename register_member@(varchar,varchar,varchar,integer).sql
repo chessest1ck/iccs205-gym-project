f42e752d79171de536d26feb0e483e8fa29b3428
cheesest1ick <ppssob2@gmail.com>
2025-11-25 00:40:14 +0700

    fix: this now accept gym_id so it can separate with other gyms

--- register_member@(varchar,varchar,varchar,integer).sql
+++ register_member@(varchar,varchar,varchar,integer).sql
@@ -1,19 +1,25 @@
-create function register_member(_name character varying, _email character varying, _phone character varying, _type_id integer) returns text
+create function register_member(_gym_id integer, _name character varying, _email character varying, _phone character varying, _type_id integer) returns text
     language plpgsql
 as
 $$
-DECLARE
-    _duration INT;
-    _end_date DATE;
+DECLARE _duration INT; _end_date DATE;
 BEGIN
-    SELECT duration_days INTO _duration FROM membership_types WHERE type_id = _type_id;
+    SELECT duration_days INTO _duration
+    FROM membership_types
+    WHERE type_id = _type_id AND gym_id = _gym_id;
+
+    IF _duration IS NULL THEN
+        RAISE EXCEPTION 'Invalid Membership Type ID % for Gym ID %', _type_id, _gym_id;
+    END IF;
+
     _end_date := CURRENT_DATE + _duration;
 
-    INSERT INTO members (full_name, email, phone, type_id, membership_end_date)
-    VALUES (_name, _email, _phone, _type_id, _end_date);
+    INSERT INTO members (gym_id, full_name, email, phone, type_id, membership_end_date)
+    VALUES (_gym_id, _name, _email, _phone, _type_id, _end_date);
+
     RETURN 'Member registered successfully. Expires on: ' || _end_date;
 END;
 $$;
 
-alter function register_member(varchar, varchar, varchar, integer) owner to root;
+alter function register_member(integer, varchar, varchar, varchar, integer) owner to root;
 
