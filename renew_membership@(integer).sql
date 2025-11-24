2b7f52543b32a04b447eb9eb8713d7b137109ec9
cheesest1ick <ppssob2@gmail.com>
2025-11-24 15:22:37 +0700

    feat: renew_membership

---
+++ renew_membership@(integer).sql
@@ -0,0 +1,43 @@
+create function renew_membership(_member_id integer) returns text
+    language plpgsql
+as
+$$
+DECLARE
+    _type_id INT;
+    _duration INT;
+    _current_end DATE;
+    _new_end DATE;
+BEGIN
+    SELECT type_id, membership_end_date
+    INTO _type_id, _current_end
+    FROM members
+    WHERE member_id = _member_id;
+
+    IF _type_id IS NULL THEN
+        RETURN 'Error: Member ID not found or has no Membership register.';
+    END IF;
+
+    SELECT duration_days INTO _duration
+    FROM membership_types
+    WHERE type_id = _type_id;
+
+    IF _duration IS NULL THEN
+        RETURN 'Error: Membership Type configuration missing.';
+    END IF;
+
+    IF _current_end IS NULL OR _current_end < CURRENT_DATE THEN
+        _new_end := CURRENT_DATE + _duration;
+    ELSE
+        _new_end := _current_end + _duration;
+    END IF;
+
+    UPDATE members
+    SET membership_end_date = _new_end
+    WHERE member_id = _member_id;
+
+    RETURN 'Success: Membership renewed until ' || _new_end;
+END;
+$$;
+
+alter function renew_membership(integer) owner to root;
+
