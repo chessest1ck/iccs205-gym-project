199142631009ac0d5c3c76506f6dfcde614fe4cc
cheesest1ick <ppssob2@gmail.com>
2025-11-25 20:00:04 +0700

    fix: logprogress now update when it is in the same day and accept gym_id

--- get_member_progress@(integer).sql
+++ get_member_progress@(integer).sql
@@ -1,19 +1,21 @@
-create function get_member_progress(_member_id integer)
-    returns TABLE(r_weight numeric, r_body_fat numeric, r_log_date date)
+create function get_member_progress(_gym_id integer, _member_id integer)
+    returns TABLE(log_date date, weight_kg numeric, body_fat_percent numeric)
     language plpgsql
 as
 $$
 BEGIN
     RETURN QUERY
     SELECT
-        p.weight_kg,
-        p.body_fat_percent,
-        p.log_date
-    FROM progress_logs AS p
-    WHERE p.member_id = _member_id
-    ORDER BY p.log_date ASC;
+        pl.log_date,
+        pl.weight_kg,
+        pl.body_fat_percent
+    FROM progress_logs pl
+    JOIN members m ON pl.member_id = m.member_id
+    WHERE pl.member_id = _member_id
+      AND m.gym_id = _gym_id
+    ORDER BY pl.log_date ASC;
 END;
 $$;
 
-alter function get_member_progress(integer) owner to root;
+alter function get_member_progress(integer, integer) owner to root;
 
--- log_progress@(integer,numeric,numeric).sql
+++ log_progress@(integer,numeric,numeric).sql
@@ -12,9 +12,13 @@ BEGIN
     END IF;
 
     INSERT INTO progress_logs (member_id, log_date, weight_kg, body_fat_percent)
-    VALUES (_member_id, CURRENT_DATE, _weight, _body_fat);
+    VALUES (_member_id, CURRENT_DATE, _weight, _body_fat)
+    ON CONFLICT (member_id, log_date)
+    DO UPDATE SET
+        weight_kg = EXCLUDED.weight_kg,
+        body_fat_percent = EXCLUDED.body_fat_percent;
 
-    RETURN 'Success: Logged weight ' || _weight || ' and body fat ' || _body_fat || ' kg for Member ' || _member_name;
+    RETURN 'Success: Progress for ' || _member_name || ' updated for today.';
 END;
 $$;
 
