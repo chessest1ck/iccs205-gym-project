3d5e75e32164e2bed5c280a3c731f4b783ff361f
cheesest1ick <ppssob2@gmail.com>
2025-11-25 19:12:09 +0700

    fix: equipment status only show for that gym

--- get_equipment_status@().sql
+++ get_equipment_status@().sql
@@ -1,5 +1,5 @@
-create function get_equipment_status()
-    returns TABLE(equipment_id integer, name text, last_maintenance_date date, status text)
+create function get_equipment_status(_gym_id integer)
+    returns TABLE(r_equipment_id integer, r_name text, r_last_maintenance date, r_status text)
     language plpgsql
 as
 $$
@@ -11,9 +11,10 @@ BEGIN
         e.last_maintenance_date,
         e.status::TEXT
     FROM equipment AS e
+    WHERE e.gym_id = _gym_id
     ORDER BY e.equipment_id;
 END;
 $$;
 
-alter function get_equipment_status() owner to root;
+alter function get_equipment_status(integer) owner to root;
 
