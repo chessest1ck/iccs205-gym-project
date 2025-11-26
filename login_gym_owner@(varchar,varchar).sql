c86ebf798538326e4eb62518691f5d7b304593bb
cheesest1ick <ppssob2@gmail.com>
2025-11-26 15:07:58 +0700

    fix: check if gym owner already have gym assign to database

--- login_gym_owner@(varchar,varchar).sql
+++ login_gym_owner@(varchar,varchar).sql
@@ -1,17 +1,18 @@
 create function login_gym_owner(_email character varying, _password_hash character varying)
-    returns TABLE(r_owner_id integer, r_full_name character varying, r_email character varying)
+    returns TABLE(r_owner_id integer, r_full_name character varying, r_email character varying, r_gym_count integer)
     language plpgsql
 as
 $$
 BEGIN
     RETURN QUERY
     SELECT
-        owner_id,
-        full_name,
-        email
-    FROM gym_owners
-    WHERE email = _email
-      AND password_hash = _password_hash;
+        go.owner_id,
+        go.full_name,
+        go.email,
+        (SELECT COUNT(*)::INT FROM gyms AS g WHERE g.owner_id = go.owner_id) AS gym_count
+    FROM gym_owners AS go
+    WHERE go.email = _email
+      AND go.password_hash = _password_hash;
 END;
 $$;
 
